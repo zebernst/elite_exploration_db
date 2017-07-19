695b1a8ed63cd43ca79f06e7370777d2d9bec823
Zach Bernstein <zacharybernstein42@me.com>
2017-07-19 16:11:04 -0500

    added correct imports in events.py

--- events.py
+++ events.py
@@ -1,4 +1,5 @@
 import dateutil.parser
+import models
 
 class JournalEvent:
     def __init__(self, event):
@@ -13,7 +14,7 @@ class EventFSDJump(JournalEvent):
     def __init__(self, event):
         super().__init__(event)
         self.jump_dist = event.get('JumpDist')
-        self.star_system = StarSystem(event.get('StarSystem'), event.get('StarPos')) # LINK TO EVENTSTARTJUMP
+        self.star_system = models.StarSystem(event.get('StarSystem'), event.get('StarPos')) # LINK TO EVENTSTARTJUMP
         self.fuel_used = event.get('FuelUsed')  
         self.fuel_level = event.get('FuelLevel')
         self.boost_used = event.get('BoostUsed')
@@ -27,7 +28,7 @@ class EventStartJump(JournalEvent):
         super().__init__(event)
         self.jump_type = event.get("JumpType")
         self.star_class = event.get("StarClass")
-        self.star_system = StarSystem(event.get('StarSystem'), self.star_class) # LINK TO EVENTFSDJUMP
+        self.star_system = models.StarSystem(event.get('StarSystem'), self.star_class) # LINK TO EVENTFSDJUMP
 
 class EventScan(JournalEvent):
     def __init__(self, event):
