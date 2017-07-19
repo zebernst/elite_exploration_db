4a4c8bb1d01dcac3f20a4cccdda2bb5c4ea9087d
Zach Bernstein <zacharybernstein42@me.com>
2017-07-19 18:22:06 -0500

    changed the models to accept the data dict directly instead of passing absurd numbers of params to __init__()

--- models.py
+++ models.py
@@ -33,49 +33,89 @@ class StarSystem(BaseModel):
 
 
 class Body(BaseModel):
-    def __init__(self):
-        self.name = #
-        self.dist_from_arrival = #in ls
-        self.semimajoraxis = #
-        self.eccentricity = #
-        self.orbit_inclination = #
-        self.periapsis = #
-        self.orbit_period = #
+    def __init__(self, data : dict):
+        self.name = data.get('BodyName')
+        self.dist_from_arrival = data.get('DistanceFromArrivalLS') #in ls
+        self.semimajoraxis = data.get('SemiMajorAxis')
+        self.eccentricity = data.get('Eccentricity')
+        self.orbit_inclination = data.get('OrbitalInclination')
+        self.periapsis = data.get('Periapsis')
+        self.orbit_period = data.get('OrbitalPeriod')
 
 
 class Star(Body):
-    def __init__(self):
-        super().__init__()
-        self.star_class = #
-        self.mass = #in solar masses
-        self.radius = #
-        self.magnitude = #
-        self.rotate_period = #
-        self.temp = #
-        self.age = #in millions of years
+    def __init__(self, data : dict):
+        super().__init__(data)
+        self.star_class = data.get('StarType')
+        self.mass = data.get('StellarMass') #in solar masses
+        self.radius = data.get('Radius')
+        self.magnitude = data.get('AbsoluteMagnitude')
+        self.rotate_period = data.get('RotationPeriod') #in seconds
+        self.temp = data.get('SurfaceTemperature')
+        self.age = data.get('Age_MY') #in millions of years
 
 
 class Planet(Body):
-    def __init__(self):
-        super().__init__()
-        self.tidal_lock = #
-        self.terraform_state = #
-        self.planet_class = #
-        self.atmosphere = #
-        self.atmosphere_type = #
-        self.atmosphere_comp = #
-        self.volcanism = #
-        self.gravity = #
-        self.temp = #
-        self.pressure = #
-        self.landable = #
-        self.materials = #
+
+    CLASSES = {
+        "metal rich body":                      1,
+        "high metal content body":              2,
+        "rocky body":                           3,
+        "icy body":                             4,
+        "rocky ice body":                       5,
+        "earthlike body":                       6,
+        "water world":                          7,
+        "ammonia world":                        8,
+        "water giant":                          9,
+        "water giant with life":                10,
+        "gas giant with water based life":      11,
+        "gas giant with ammonia based life":    12,
+        "sudarsky class i gas giant":           13,
+        "sudarsky class ii gas giant":          14,
+        "sudarsky class iii gas giant":         15,
+        "sudarsky class iv gas giant":          16,
+        "sudarsky class v gas giant":           17,
+        "helium rich gas giant":                18,
+        "helium gas giant":                     19 }
+    TERRAFORM_STATES = {
+        'Null': 0,
+        'Terraformable': 1,
+        'Terraforming':  2,
+        'Terraformed':   3 }
+    ATMOSPHERES = {}
+    VOLCANISM_TYPES = {}
+
+    def __init__(self, data : dict):
+        super().__init__(data)
+        self.tidal_lock = data.get('TidalLock')
+        self.terraform_state = self.TERRAFORM_STATES.get(data.get('TerraformState'))
+        self.planet_class = self.CLASSES.get(data.get('PlanetClass'))
+        self.atmosphere = data.get('Atmosphere') # TODO
+        self.atmosphere_type = data.get('AtmosphereType') # TODO
+        self.atmosphere_comp = data.get('AtmosphereComposition') # TODO
+        self.volcanism = data.get('Volcanism') # TODO
+        self.gravity = data.get('SurfaceGravity')
+        self.temp = data.get('SurfaceTemperature')
+        self.pressure = data.get('SurfacePressure')
+        self.landable = data.get('Landable')
+        self.materials = data.get('Materials') # TODO
+        self.mass = data.get('MassEM') # in earth masses
+        self.radius = data.get('Radius') #
+        # TODO: change get() to pop() and print remainder of dict to make sure im storing everything
 
 class Ring(BaseModel):
-    def __init__(self):
-        self.name = #
-        self.ring_class = #
-        self.mass = #in megatons
-        self.inner_radius = #
-        self.outer_radius = #
-        self.reserve_level = #technically a planet attribute but whatever
+
+    RESERVE_LEVELS = {
+        "PristineResources": 0,
+        "MajorResources": 1,
+        "CommonResources": 2,
+        "LowResources": 3,
+        "DepletedResources": 4 }
+
+    def __init__(self, data : dict, reserves):
+        self.name = data.get('Name')
+        self.ring_class = data.get('RingClass')
+        self.mass = data.get('MassMT') #in megatons
+        self.inner_radius = data.get('InnerRad')
+        self.outer_radius = data.get(OuterRad)
+        self.reserve_level = RESERVE_LEVELS.get(reserves)
