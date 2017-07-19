ddcb6c2fb143c36db4ac19f2a005da10c3cd72cc
Zach Bernstein <zacharybernstein42@me.com>
2017-07-19 16:34:07 -0500

    added more attributes to body models

--- models.py
+++ models.py
@@ -2,17 +2,16 @@ from peewee import *
 
 db = SqliteDatabase('exploration.db')
 
+class BaseModel(Model):
+    class Meta:
+        database = db
 
-class StarSystem(Model):
+class StarSystem(BaseModel):
     name = CharField()
     star_class = CharField()
     pos_x = FloatField()
     pos_y = FloatField()
     pos_z = FloatField()
-
-    class Meta:
-        database = db
-
 
     def __init__(self, name, star_class=None, coords=None):
         # self.sector = # logic for extracting sector name
@@ -33,14 +32,50 @@ class StarSystem(Model):
         return [self.pos_x, self.pos_y, self.pos_z]
 
 
-class Body:
-    pass
+class Body(BaseModel):
+    def __init__(self):
+        self.name = #
+        self.dist_from_arrival = #in ls
+        self.semimajoraxis = #
+        self.eccentricity = #
+        self.orbit_inclination = #
+        self.periapsis = #
+        self.orbit_period = #
+
 
 class Star(Body):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.star_class = #
+        self.mass = #in solar masses
+        self.radius = #
+        self.magnitude = #
+        self.rotate_period = #
+        self.temp = #
+        self.age = #in millions of years
+
 
 class Planet(Body):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.tidal_lock = #
+        self.terraform_state = #
+        self.planet_class = #
+        self.atmosphere = #
+        self.atmosphere_type = #
+        self.atmosphere_comp = #
+        self.volcanism = #
+        self.gravity = #
+        self.temp = #
+        self.pressure = #
+        self.landable = #
+        self.materials = #
 
-class Ring:
-    pass
+class Ring(BaseModel):
+    def __init__(self):
+        self.name = #
+        self.ring_class = #
+        self.mass = #in megatons
+        self.inner_radius = #
+        self.outer_radius = #
+        self.reserve_level = #technically a planet attribute but whatever
