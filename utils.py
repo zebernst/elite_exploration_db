96683b6555aba0b34233d8d597afe777381b438c
Zach Bernstein <zacharybernstein42@me.com>
2017-07-19 16:37:10 -0500

    added a few basic function scaffolds to utils.py

--- utils.py
+++ utils.py
@@ -1,2 +1,12 @@
 def split_sysname(namestr):
+	# split system name into sector and system name, also maybe give info about what the name means??
 	pass
+
+def solarmasses_to_kg(stellar_mass):
+	# convert stellar masses from multiples of the sun's mass to kilograms
+	pass
+
+def megatons_to_kg(mass_mt):
+	# convert ring mass from megatons to kilograms
+	pass
+	
